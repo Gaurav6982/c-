bcfecf2f5c4508862a36c8d55510ee9806c24303
Gaurav6982 <gaurav.jss.027@gmail.com>
2020-10-24 22:09:29 +0530

    Coin Combin 2

--- CSES/DP/coin_combinations2.cpp
+++ CSES/DP/coin_combinations2.cpp
@@ -1,37 +1,34 @@
 #include<bits/stdc++.h>
-
+ 
 #define ll long long
 #define mod 1000000007
-#define rep(i,n) for(ll i=0;i<n;i++)
-
+#define rep(i,n) for(int i=0;i<n;i++)
+ 
 using namespace std;
-ll dp[101][1000001];
-ll find(int arr[],int n,int x)
-{
-    if(x==0)
-    return 1;
-    if(n==0)
-    return 0;
-    if(dp[n][x]!=-1)
-    return dp[n][x];
-    ll ans=0;
-    if(arr[n-1]<=x)
-    ans=(find(arr,n-1,x)%mod)+(find(arr,n,x-arr[n-1])%mod);
-    else
-    ans=find(arr,n-1,x)%mod;
-    return dp[n][x]=ans%mod;
-    // return ans;
-}
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int n,x;
+    vector<int> arr(n);
     cin>>n>>x;
-    int arr[n];
     rep(i,n) cin>>arr[i];
-    // sort(arr,arr+n);
-    rep(i,101) {rep(j,1000001) dp[i][j]=-1;}
-    cout<<find(arr,n,x)<<endl;
-    return 0;
+ 
+    int dp[n+1][x+1];
+    rep(i,n+1) dp[i][0]=1;
+    rep(i,x+1) dp[0][i]=0;
+    
+    for(int i=1;i<n+1;i++)
+    {
+        for(int j=1;j<x+1;j++)
+        {
+            if(arr[i-1]<=j)
+            dp[i][j]=(dp[i-1][j]+dp[i][j-arr[i-1]])%mod;
+            else
+            dp[i][j]=dp[i-1][j]%mod;
+        }
+    }
+    cout<<dp[n][x];
+ 
 }
