13235a7996e9c40bf15b1f08acd47dda309991a9
Gaurav6982 <gaurav.jss.027@gmail.com>
2020-11-30 11:15:15 +0530

    Added Code

--- Try/Nov/travellingTom.cpp
+++ Try/Nov/travellingTom.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;i++)
 using namespace std;
 struct edge{
     int u,v,l;
@@ -8,6 +9,37 @@ struct edge{
 bool comp(edge a,edge b)
 {
     return a.w<b.w;
+}
+int parent[100001];
+int rnk[100001];
+int find(int a){
+    if(parent[a]==a)
+    return a;
+    return parent[a]=find(parent[a]);
+}
+void join(int a,int b){
+    if(rnk[a]<rnk[b])
+    swap(a,b);
+    parent[b]=a;
+    if(rnk[a]==rnk[b])
+    rnk[a]++;
+}
+int num_edges=0;
+void kruskal(edge edges[],int m,int cost[],int k,int needed[]){
+    
+    for (int i = 0; i < m; i++)
+    {
+        int u=edges[i].u-1;
+        int v=edges[i].v-1;
+        int w=edges[i].w;
+        if(find(u)!=find(v))
+        {
+            num_edges++;
+            join(u,v);
+            needed[u]=needed[v]=1;        
+        }
+    }
+    
 }
 int main(){
     int n,m,k;
@@ -31,9 +63,27 @@ int main(){
         edges[i].w=weight;
     }
     sort(edges,edges+m,comp);
-    for(int i=0;i<m;i++)
+    int needed[k];
+    rep(i,k) needed[i]=0;
+    rep(i,n+1) parent[i]=i,rnk[i]=0;
+    kruskal(edges,m,cost,k,needed);
+    int ans=0;
+    for (int i = 0; i < k; i++)
     {
-        cout<<"Edge "<<i+1<<" : "<<edges[i].u<<" "<<edges[i].v<<" "<<edges[i].w<<endl;
+        // cout<<needed[i]<<" ";
+        if(needed[i]==1)
+            ans+=cost[i];
     }
+    if(num_edges==n-1)
+    cout<<ans<<endl;
+    else
+    {
+        cout<<1<<endl;
+    }
+    
+    // for(int i=0;i<m;i++)
+    // {
+    //     cout<<"Edge "<<i+1<<" : "<<edges[i].u<<" "<<edges[i].v<<" "<<edges[i].w<<endl;
+    // }
     return 0; 
 }
