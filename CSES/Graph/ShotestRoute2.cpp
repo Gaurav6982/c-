d8afdb8eb9446b0a133199a3e9cd06ecb889ed19
Gaurav6982 <gaurav.jss.027@gmail.com>
2020-12-03 20:06:52 +0530

    Shortest ROute - 2 Floyd Warshell - for negative cycles

--- CSES/Graph/ShotestRoute2.cpp
+++ CSES/Graph/ShotestRoute2.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 
 #define ll long long
+#define MAX_VAL 1e17
 #define mod 1000000007
 #define rep(i,n) for(int i=0;i<n;i++)
 
@@ -13,7 +14,7 @@ void floyd(int n,int m,vector<ll> dp[])
         {
             for(int k=0;k<n+1;k++)
             {
-                if(dp[j][i]<LONG_MAX && dp[i][k]<LONG_MAX)
+                if(dp[j][i]<MAX_VAL && dp[i][k]<MAX_VAL)
                 dp[j][k]=min(dp[j][k],dp[j][i]+dp[i][k]);
             }
         }
@@ -28,17 +29,17 @@ int main(){
     vector<ll> dp[n+1];
     for(int i=0;i<n+1;i++)
     {
-        dp[i]=vector<ll>(n+1,LONG_MAX);
+        dp[i]=vector<ll>(n+1,MAX_VAL);
         dp[i][i]=0;
     }
     while(m--)
     {
-        int a,b;
+        ll a,b;
         ll c;
         cin>>a>>b>>c;
         if(c<dp[a][b])
         dp[a][b]=c;
-        if(c<dp[b][a]);
+        if(c<dp[b][a])
         dp[b][a]=c;   
     }
     
@@ -48,7 +49,7 @@ int main(){
         int a ,b;
 
         cin>>a>>b;
-        if(dp[a][b]!=LONG_MAX)
+        if(dp[a][b]!=MAX_VAL)
         cout<<dp[a][b]<<endl;
         else 
         cout<<-1<<endl;
