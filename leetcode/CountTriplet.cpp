31a487b0058fbbaf5f356a1d28a219609502058d
Gaurav6982 <gaurav.jss.027@gmail.com>
2020-12-24 17:11:11 +0530

    practice

--- leetcode/CountTriplet.cpp
+++ leetcode/CountTriplet.cpp
@@ -27,3 +27,31 @@ public:
         return ans;
     }
 };
+
+//faster
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> scs(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            scs[i]=scs[i-1]^arr[i-1];
+        }
+        // for(int i=0;i<=n;i++)
+        //     cout<<scs[i]<<" ";
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n-1;j>i;j--)
+            {
+                int left=scs[j+1]^scs[i];
+                
+                if(left==0)
+                    ans+=j-i;
+            }
+        }
+        return ans;
+    }
+};
